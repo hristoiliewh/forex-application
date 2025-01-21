9d55380e8601a2a1309cd07ad8fd519652ee83bc
hristoiliew <hristoiliewh@gmail.com>
2025-01-21 20:59:12 +0200

    Initial project setup

---
+++ src/test/java/com/forex/forex_application/ForexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forex.forex_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
